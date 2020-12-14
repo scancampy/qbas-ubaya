490c0cf72a417546aba098d8a4f5e7d660fb9fd7
scancampy <andre@staff.ubaya.ac.id>
2020-12-14 22:36:40 +0700

    small fix

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -18,4 +18,34 @@ class Api extends CI_Controller {
 		}
 	}
 
+	public function getserverdate() {
+		echo json_encode(array('serverdate' => strftime("%A, %d %B %Y", strtotime("now"))));
+	}
+
+	public function getstudent() {
+		if($this->input->post('nrp')) {
+			$result = $this->student_model->getStudent($this->input->post('nrp'));
+
+			if($result == false) {
+				echo json_encode(array('result'=> false));
+			} else {
+				echo json_encode(array('result' => true, 'data' => $result ));
+			}
+		} else {
+			echo json_encode(array('result'=> false));
+		}
+	}
+
+	public function getcurrentclass() {
+		if($this->input->post('nrp')) {
+			$result = $this->attendances_model->getCurrentClass($this->input->post('nrp'));
+			if($result == false) {
+				echo json_encode(array('result'=> false));
+			} else {
+				echo json_encode(array('result' => true, 'data' => $result ));
+			}
+		} else {
+			echo json_encode(array('result'=> false));
+		}
+	}
 }
--- application/models/Student_model.php
+++ application/models/Student_model.php
@@ -3,6 +3,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Student_model extends CI_Model {
 
+	public function getStudent($nrp) {
+		$result = $this->db->get_where('student', array('nrp' => $nrp));
+		if($result->num_rows() >0) {
+			return $result->row();
+		} else {
+			return false;
+		}
+	}
+
 	public function doSignIn($nrp, $password) {
 		$newNrp = substr($nrp, 1);
 		$res = $this->db->get_where('student', array('nrp' => $newNrp));
